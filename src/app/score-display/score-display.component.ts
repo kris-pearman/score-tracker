0007963e6e05dc71f870bdbcf7626b94a8338bf4
kris-pearman <57414402+kris-pearman@users.noreply.github.com>
2022-05-19 23:08:20 +0100

    Reading from JSON file

--- src/app/score-display/score-display.component.ts
+++ src/app/score-display/score-display.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SCORES } from '../data/mock-scores';
+import {default as data} from '../data/Stats.json';
 
 @Component({
   selector: 'app-score-display',
@@ -9,10 +10,23 @@ import { SCORES } from '../data/mock-scores';
 export class ScoreDisplayComponent implements OnInit {
   scores = SCORES;
   
+
+  
   constructor() { }
 
   ngOnInit(): void {
-  
+    // console.log(data.Stats.SongScores)
+    this.getScores(data.Stats.SongScores.Song)
   }
+
+  getScores(stats: any) {
+        for (var val of stats) {
+          // console.log(val['-Dir'])
+          console.log(val.Steps.HighScoreList)
+          // console.log(val)
+        }
+        
+  }
+
 
 }
